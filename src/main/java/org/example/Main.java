fb607166830ec758b031abcbb4745df6cd5b274f
Invader6666 <aefranko123@gmail.com>
2026-05-09 02:40:09 +0300

    Inquiry Service Implementation

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,23 @@
 package org.example;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 public class Main {
+
     public static void main(String[] args) {
+
+
+
+        InquiryService obj = new InquiryService();
+        obj.GetTopCropType();
+        obj.GetInactiveFarmsLastMonth();
+        obj.GetTopDriverLastMonth();
+        obj.GetRestaurantsWithoutOrders();
+        obj.GetRestaurantDeliveredBatches();
+        obj.GetFarmRevenue();
+
 
     }
 }
